6ff5e211cb4923f93f161eef852d9fe03ab8755d
LinoB <77000248+LinoBellanca@users.noreply.github.com>
2024-10-15 22:53:28 +0200

    FSM : State Parent

---
+++ Source/GD3_Lino_Gilbert/Public/State.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "State.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GD3_LINO_GILBERT_API UState : public UObject
+{
+	GENERATED_BODY()
+	
+protected:
+	virtual void EntryState();
+	virtual void UpdateState();
+	virtual void ExitState();
+};
